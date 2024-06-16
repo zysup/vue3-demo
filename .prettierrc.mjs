11b4b304d2f90d458523d486c535ec9c8812be83
ck <ck@ck.com>
2024-06-16 17:34:12 +0800

    first commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+/**
+ * 修改配置后重启编辑器
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true, // 使用单引号代替双引号
+  semi: false, // 每行末尾自动添加分号
+  printWidth: 120, // 换行长度，默认80
+}
+export default config
