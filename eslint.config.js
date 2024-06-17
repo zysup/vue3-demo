1822673bb313c3209c304f56fd29ede73367831f
ck <ck@ck.com>
2024-06-17 23:25:04 +0800

    qwe

--- eslint.config.js
+++ eslint.config.js
@@ -6,17 +6,23 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
 
-// console.log('qwe', ...tseslint.configs.recommended)
-
 export default [
-  // { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   {
+    ignores: ['public', 'dist', 'node_modules'],
     rules: {
-      // 'no-unused-vars': 'warn',
-      // 'no-undef': 'warn',
+      // eslint（https://eslint.org/docs/latest/rules/）
+      'no-useless-escape': 'off', // 禁止不必要的转义字符
+
+      // typeScript (https://typescript-eslint.io/rules)
+      '@typescript-eslint/no-explicit-any': 'off', // 禁止使用 any 类型
+      '@typescript-eslint/no-namespace': 'off', // 禁止使用自定义 TypeScript 模块和命名空间。
+
+      // eslint-plugin-vue (https://eslint.vuejs.org/rules/)
+      'vue/multi-word-component-names': 'off', // 要求组件名称始终为 “-” 链接的单词
     },
   },
 ]
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue'
 
-defineProps<{ msg: string }>();
-
-const count = ref(0);
-
-const str = "qwe";
+defineProps({
+  msg: String,
+})
+const count = ref(0)
 </script>
 
 <template>
@@ -21,9 +20,8 @@ const str = "qwe";
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
