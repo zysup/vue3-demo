4127a4fb12b466db85237ca00725187362299982
ck <ck@ck.com>
2024-06-18 21:53:43 +0800

    123

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  // { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/essential'],
