d8eb62db8d07f621f4b4dc39ec8aa096e1dd6d7a
ck <ck@ck.com>
2024-06-19 23:14:17 +0800

    sadf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,14 +12,20 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p class="bg-blue-500 pt-8 text-base font-semibold leading-7 text-red-400">
-      Edit
-      <code
-        class="my-code bg-green-700 px-4 py-1 pt-8 text-base font-semibold leading-7 text-red-400 hover:bg-sky-800 sm:px-8 sm:py-3"
-        >compon ents/HelloWorld.vue</code
-      >
-      to test "HMR"
-    </p>
+    <p class="bg-blue-500 bg-clip-text pt-8 text-base font-semibold leading-7 text-red-400">Edit</p>
+    <code
+      class="my-code bg-green-700 px-20 py-1 pt-4 text-base font-semibold leading-7 text-red-400 hover:bg-sky-800 sm:px-8 sm:py-3"
+    >
+      compon ents/HelloWorld.vue
+      <span>123</span>
+    </code>
+    to test "HMR"
+    <span>567</span>
+    <ul>
+      <li>01</li>
+      <li>02</li>
+      <li>03</li>
+    </ul>
   </div>
 
   <p>
@@ -33,13 +39,24 @@ const count = ref(0)
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="bg-sky-500 hover:bg-sky-700 landscape:hidden">asdf</p>
+  <p class="border-red-700 empty:border"></p>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .read-the-docs {
   color: #888;
 }
 .my-code {
-  @apply bg-red-500 !important;
+  @apply bg-red-500 #{!important};
+  span {
+    color: yellow;
+  }
+}
+.bg-sky-500 {
+  background-color: #0ea5e9;
+}
+.hover\:bg-sky-700:hover {
+  background-color: red;
 }
 </style>
