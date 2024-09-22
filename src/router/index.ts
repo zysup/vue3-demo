2f80b564bcef972159be2158a29a70ec83a41320
ck <ck@ck.com>
2024-09-22 21:16:11 +0800

    asdf

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,25 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, type RouteLocationNormalized } from 'vue-router'
 import routes from './routes'
 import NProgress from '@/utils/progress'
+import setPageTitle from '@/utils/set-page-title'
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
 })
 
-router.beforeEach((_to, _from, next) => {
+export interface toRouteType extends RouteLocationNormalized {
+  meta: {
+    title?: string
+  }
+}
+
+router.beforeEach((to: toRouteType, _, next) => {
   NProgress.start()
   // 路由缓存
   // useCachedViewStoreHook().addCachedView(to);
   // 页面 title
-  // setPageTitle(to.meta.title);
+  setPageTitle(to.meta.title)
   next()
 })
 
