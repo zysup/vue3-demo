6d9f904712f19238a58985a16a2995958b4eac5a
ck <ck@ck.com>
2024-12-11 23:59:37 +0800

    qwe

--- src/pages/Authorizedlogin/index.vue
+++ src/pages/Authorizedlogin/index.vue
@@ -7,11 +7,46 @@
   <div class="page-authorized-login">
     <van-nav-bar title="Authorizedlogin" left-arrow @click-left="onClickLeft" />
     <van-button type="primary" onclick="checkLoginState()">Facebook Login</van-button>
-    <!-- <van-button type="primary" onclick="checkLoginState()">Google Login</van-button> -->
+    <van-divider />
+    <van-button type="primary" onclick="initializeGoogleSignIn()">Google Login</van-button>
     <div id="google-login-button"></div>
+    <van-divider />
+    <van-button type="primary" @click="hellojsLogin">Google Login</van-button>
   </div>
 </template>
 
 <script setup lang="ts">
+const GOOGLE_CLIENT_ID = '278327607206-0i9241asfd41bcd2ueovopsq71c7h083.apps.googleusercontent.com'
+import hellojs from 'hellojs/dist/hello.all.js'
+hellojs.init(
+  {
+    google: GOOGLE_CLIENT_ID,
+  },
+  { redirect_uri: 'https://zysup.github.io' },
+)
+
 const onClickLeft = () => history.back()
+
+const hellojsLogin = () => {
+  hellojs('google')
+    .login()
+    .then(
+      function () {
+        alert('You are signed in to Facebook')
+        hellojs('facebook')
+          .api('me')
+          .then(
+            function (json) {
+              alert('Your name is ' + json.name)
+            },
+            function (e) {
+              alert('Whoops! ' + e.error.message)
+            },
+          )
+      },
+      function (e) {
+        alert('Signin error: ' + e.error.message)
+      },
+    )
+}
 </script>
--- src/utils/googleLogin.js
+++ src/utils/googleLogin.js
@@ -1,14 +1,22 @@
+import { showToast } from 'vant'
+
 const clientId = '278327607206-0i9241asfd41bcd2ueovopsq71c7h083.apps.googleusercontent.com'
 const script = document.createElement('script')
 const handleGoogleSignIn = (response) => {
   // 处理Google登录成功的响应
   console.log('Google登录成功', response)
+  let strings = response.credential.split('.') //截取token，获取载体
+  var userinfo = JSON.parse(decodeURIComponent(escape(window.atob(strings[1].replace(/-/g, '+').replace(/_/g, '/'))))) //解析，需要吧‘_’,'-'进行转换否则会无法解析
+
+  console.log('qwe', userinfo)
+  showToast({ message: '登录成功 ' + JSON.stringify(userinfo, null, 2), duration: 4000 })
 }
 const handleGoogleSignInCancel = () => {
   // 处理Google登录取消
   console.log('Google登录被取消')
 }
-const initializeGoogleSignIn = (client_id) => {
+window.initializeGoogleSignIn = (client_id = clientId) => {
+  console.log('qwe', client_id)
   window.google.accounts.id.initialize({
     client_id,
     cancel_on_tap_outside: true, // 控制是否在提示之外进行点击时取消提示(关闭一键登录弹窗)，默认true
@@ -17,20 +25,31 @@ const initializeGoogleSignIn = (client_id) => {
     cancel: handleGoogleSignInCancel,
   })
   // 渲染“使用 Google 帐号登录”按钮
-  window.google.accounts.id.renderButton(document.getElementById('google-login-button'), {
-    theme: 'outline',
-    size: 'large',
-    text: 'login_with',
-    shape: 'rectangular',
-  })
+  // window.google.accounts.id.renderButton(document.getElementById('google-login-button'), {
+  //   theme: 'outline',
+  //   size: 'large',
+  //   text: 'login_with',
+  //   shape: 'rectangular',
+  // })
   // 启用一键登录提示(弹窗)功能
-  window.google.accounts.id.prompt()
+  window.google.accounts.id.prompt((notification) => {
+    console.log('qwe notification', notification)
+    if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+      console.log('qwe notification.isNotDisplayed()', notification.isNotDisplayed())
+      console.log('qwe notification.isSkippedMoment()', notification.isSkippedMoment())
+      // continue with another identity provider.
+    }
+  })
 }
 if (window.google) {
-  initializeGoogleSignIn(clientId)
+  // initializeGoogleSignIn(clientId)
 } else {
   script.src = 'https://accounts.google.com/gsi/client' // 加载客户端库
   script.async = true
-  script.onload = () => initializeGoogleSignIn(clientId)
+  // script.onload = () => initializeGoogleSignIn(clientId)
+  // script.onload = () => {
+  //   console.log('google 登录sdk加载完成')
+  //   initializeGoogleSignIn()
+  // }
   document.head.appendChild(script)
 }
