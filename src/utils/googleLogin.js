ecfe9a57bdef0918172a7fcb53af1e801346805c
ck <ck@ck.com>
2024-12-14 18:16:44 +0800

    asdf

--- src/utils/googleLogin.js
+++ src/utils/googleLogin.js
@@ -1,7 +1,7 @@
 import { showToast } from 'vant'
 
 const clientId = '278327607206-0i9241asfd41bcd2ueovopsq71c7h083.apps.googleusercontent.com'
-const clientSecret = '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'
+const clientSecret = 'GOCSPX-HfnrHnAalpfc0_' + 'bklzmeGEAyT-LT'
 const redirectUri = 'https://zysup.github.io'
 const script = document.createElement('script')
 const handleGoogleSignIn = (response) => {
