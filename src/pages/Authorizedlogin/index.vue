87dcc70f2f93628f91abf948c108fe87fdeed2c8
ck <ck@ck.com>
2024-12-14 18:11:33 +0800

    qwe

--- src/pages/Authorizedlogin/index.vue
+++ src/pages/Authorizedlogin/index.vue
@@ -11,42 +11,7 @@
     <van-button type="primary" onclick="initializeGoogleSignIn()">Google Login</van-button>
     <div id="google-login-button"></div>
     <van-divider />
-    <van-button type="primary" @click="hellojsLogin">Google Login</van-button>
   </div>
 </template>
 
-<script setup lang="ts">
-const GOOGLE_CLIENT_ID = '278327607206-0i9241asfd41bcd2ueovopsq71c7h083.apps.googleusercontent.com'
-import hellojs from 'hellojs/dist/hello.all.js'
-hellojs.init(
-  {
-    google: GOOGLE_CLIENT_ID,
-  },
-  { redirect_uri: 'https://zysup.github.io' },
-)
-
-const onClickLeft = () => history.back()
-
-const hellojsLogin = () => {
-  hellojs('google')
-    .login()
-    .then(
-      function () {
-        alert('You are signed in to Facebook')
-        hellojs('facebook')
-          .api('me')
-          .then(
-            function (json) {
-              alert('Your name is ' + json.name)
-            },
-            function (e) {
-              alert('Whoops! ' + e.error.message)
-            },
-          )
-      },
-      function (e) {
-        alert('Signin error: ' + e.error.message)
-      },
-    )
-}
-</script>
+<script setup></script>
--- src/utils/googleLogin.js
+++ src/utils/googleLogin.js
@@ -1,6 +1,8 @@
 import { showToast } from 'vant'
 
 const clientId = '278327607206-0i9241asfd41bcd2ueovopsq71c7h083.apps.googleusercontent.com'
+const clientSecret = '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'
+const redirectUri = 'https://zysup.github.io'
 const script = document.createElement('script')
 const handleGoogleSignIn = (response) => {
   // 处理Google登录成功的响应
@@ -16,7 +18,6 @@ const handleGoogleSignInCancel = () => {
   console.log('Google登录被取消')
 }
 window.initializeGoogleSignIn = (client_id = clientId) => {
-  console.log('qwe', client_id)
   window.google.accounts.id.initialize({
     client_id,
     cancel_on_tap_outside: true, // 控制是否在提示之外进行点击时取消提示(关闭一键登录弹窗)，默认true
@@ -34,10 +35,11 @@ window.initializeGoogleSignIn = (client_id = clientId) => {
   // 启用一键登录提示(弹窗)功能
   window.google.accounts.id.prompt((notification) => {
     console.log('qwe notification', notification)
-    if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
-      console.log('qwe notification.isNotDisplayed()', notification.isNotDisplayed())
-      console.log('qwe notification.isSkippedMoment()', notification.isSkippedMoment())
-      // continue with another identity provider.
+    console.log('qwe notification.isNotDisplayed()', notification.isNotDisplayed())
+    console.log('qwe notification.isSkippedMoment()', notification.isSkippedMoment())
+    if (notification.g === 'skipped' && notification.l === 'unkown_reason') {
+      console.log('qwe 触发手动登录')
+      manualLogin()
     }
   })
 }
@@ -46,10 +48,49 @@ if (window.google) {
 } else {
   script.src = 'https://accounts.google.com/gsi/client' // 加载客户端库
   script.async = true
-  // script.onload = () => initializeGoogleSignIn(clientId)
   // script.onload = () => {
   //   console.log('google 登录sdk加载完成')
   //   initializeGoogleSignIn()
   // }
   document.head.appendChild(script)
 }
+/**
+ * 跳转到授权页面,触发手动登录
+ */
+function manualLogin() {
+  const ahref = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/drive.metadata.readonly&include_granted_scopes=true&response_type=code&state=abc&redirect_uri=${redirectUri}&client_id=${clientId}&prompt=consent`
+  const aEle = document.createElement('a')
+  aEle.href = ahref
+  aEle.click()
+}
+// 授权后的重定向逻辑处理,通过code和客户端秘钥获取用户信息
+function handleRedirectPageLogin() {
+  const _p = new URLSearchParams(location.search)
+  const code = _p.get('code')
+  // const state = _p.get('state')
+  if (!code) return
+  const myHeaders = new Headers()
+  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow',
+  }
+
+  fetch(
+    `https://oauth2.googleapis.com/token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${redirectUri}`,
+    requestOptions,
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      const userinfo = JSON.parse(
+        decodeURIComponent(escape(window.atob(result.id_token.split('.')[1].replace(/-/g, '+').replace(/_/g, '/')))),
+      )
+      console.log(result)
+      showToast({ message: '登录成功 ' + JSON.stringify(userinfo, null, 2), duration: 4000 })
+    })
+    .catch((error) => console.error(error))
+}
+
+handleRedirectPageLogin()
