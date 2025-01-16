7709e0e6c161b2872ac3c0af80f0fc4dbbed56c9
ck <ck@ck.com>
2025-01-16 21:49:23 +0800

    qwe

--- src/pages/VantFeature/index.vue
+++ src/pages/VantFeature/index.vue
@@ -18,6 +18,32 @@
     <div class="van-safe-area-bottom weq">qwe</div>
 
     <div class="van-haptics-feedback" style="background-color: red">adasd</div>
+
+    <br />
+    <van-tabs v-model:active="active" swipeable>
+      <van-tab v-for="index in 4" :title="'标签 ' + index">
+        <div style="height: 100px; background-color: cadetblue">
+          <p>内容 {{ index }}</p>
+          <!-- <div style="width: 100%; overflow: hidden" @scroll.stop> -->
+          <ul class="tab-ul" @touchmove.stop>
+            <li>01</li>
+            <li>02</li>
+            <li>03</li>
+            <li>04</li>
+            <li>05</li>
+          </ul>
+          <!-- </div> -->
+        </div>
+      </van-tab>
+    </van-tabs>
+
+    <ul class="tab-ul">
+      <li>01</li>
+      <li>02</li>
+      <li>03</li>
+      <li>04</li>
+      <li>05</li>
+    </ul>
   </div>
 </template>
 <script setup lang="ts" name="VantFeature">
@@ -38,4 +64,34 @@ showFailToast({
   icon: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
   iconSize: '2em',
 })
+
+const active = ref(1)
 </script>
+
+<style scoped lang="scss">
+.tab-ul {
+  outline: 1px solid red;
+  outline-offset: -2px;
+  width: 300px;
+  overflow-x: scroll;
+  white-space: nowrap;
+  li {
+    display: inline-block;
+    height: 50px;
+    width: 100px;
+    background-color: burlywood;
+    vertical-align: top;
+  }
+}
+</style>
+<style>
+.tab-ul {
+  /* overflow: hidden; 隐藏滚动条 */
+  /* -ms-overflow-style: none; 适用于 Internet Explorer 和 Edge */
+  scrollbar-width: none; /*适用于 Firefox*/
+}
+
+.tab-ul::-webkit-scrollbar {
+  display: none; /* 适用于 Chrome, Safari 和 Opera */
+}
+</style>
